d3dac8ca7ebe25cb8eec2bafed3ab02ff6654406
Ed Baker <edwbaker@gmail.com>
2016-10-07 11:50:27 +0100

    Update bioacoustica.R

--- R/bioacoustica.R
+++ R/bioacoustica.R
@@ -69,9 +69,7 @@ bioacoustica.listRecordings <- function(taxon=NULL, children=FALSE, skipcheck=TR
     path <- paste0("/R/recordings/", taxon);
     recordings <- bioacoustica.call(path);
   } else {
-    #TODO:Get taxon ID
-    tid <- 262;
-    path <- paste0("/R/recordings-depth/", tid);
+    path <- paste0("/R/recordings-depth/", taxon);
     recordings <- bioacoustica.call(path);
   }
   return (recordings);
