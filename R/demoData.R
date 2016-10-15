ca44807abd03d9de82dd19ef55076588190d22ab
Ed Baker <edwbaker@gmail.com>
2016-10-15 18:27:29 +0100

    Update demoData.R

--- R/demoData.R
+++ R/demoData.R
@@ -9,20 +9,30 @@ bioacoustica.britishOrthoptera <- list(
   decticus.verrucivorus = list(
     204,
     205
+  ),
+  pholidoptera.griseoaptera = list(
+    207,
+    209
+  ),
+  platycleis.albopunctata = list(
+    211
   )
 );
 
 bioacoustica.downloadDemoData <- function(demo) {
+  bioacoustica.mkdir("data")
+  setwd("./data")
   foreach(i=1:length(demo)) %do% bioacoustica.downloadDemoDatum(demo[i])
 }
 
 bioacoustica.downloadDemoDatum <- function(datum, sep_dirs=TRUE) {
   if (sep_dirs==TRUE) {
     bioacoustica.mkdir(names(datum));
+    setwd(paste0("./",names(datum)))
   }
-  setwd(paste0("./",names(datum)))
   foreach(i=1:length(datum[[1]])) %do% bioacoustica.getAnnotationFile(as.numeric(datum[[1]][i]))
   if (sep_dirs==TRUE) {
     setwd("./..")
   }
+  setwd("./..")
 }
