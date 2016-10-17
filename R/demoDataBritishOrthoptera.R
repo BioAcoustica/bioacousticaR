946f11c0202fe30fe5c04d6a0366d924cdf713d2
Ed Baker <edwbaker@gmail.com>
2016-10-17 17:22:08 +0100

    Create demoDataBritishOrthoptera.R

---
+++ R/demoDataBritishOrthoptera.R
@@ -0,0 +1,46 @@
+bioacoustica.britishOrthoptera <- list(
+  meconema.thalassinum = list(
+    197,
+    200
+  ),
+  tettigonia.viridissima = list(
+    202
+  ),
+  decticus.verrucivorus = list(
+    204,
+    205
+  ),
+  pholidoptera.griseoaptera = list(
+    207,
+    209
+  ),
+  platycleis.albopunctata = list(
+    211
+  ),
+  metrioptera.brachyptera = list(
+    213,
+    218
+  ),
+  roeseliana.roeselii = list(
+    220,
+    222
+  ),
+  conocephalus.fuscus = list(
+    224,
+    225
+  ),
+  conocephalus.dorsalis = list(
+    227
+  ),
+  gryllus.campestris = list(
+    229
+  ),
+  nemobius.sylvestris = list(
+    231
+  ),
+  gryllotalpa.gryllotalpa = list(
+    232,
+    46,
+    233
+  )
+);
