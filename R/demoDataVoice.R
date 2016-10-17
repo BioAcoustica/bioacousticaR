0267048f569a7d0037e27afc81d6728c01af6248
Ed Baker <edwbaker@gmail.com>
2016-10-17 18:09:03 +0100

    Update demoDataVoice.R

--- R/demoDataVoice.R
+++ R/demoDataVoice.R
@@ -1,27 +1,56 @@
 bioacoustica.britishOrthoptera <- list(
   price = list(
-    235,
-    80,
-    236,
-    237,
-    238,
-    109,
-    101,
-    95,
-    89,
-    72
+    c(
+      235, 
+      "16th December. Recording number 4."
+    ),
+    c(
+      80,
+      "8th December. Trafalgar."
+    ),
+    c(
+      236,
+      "6th December.Recording number 1."
+    ),
+    c(
+      237,
+      "20th December. Recording number 2. Queenstown. Platypleura brunea."
+    ),
+    c(
+      238,
+      "Leisure Bay. Recording number 1."
+    ),
+    c(
+      109
+      "14th December. Recording number 3 on the way to Kruger." 
+    ),
+    c(
+      101,
+      "11th December. On the way to Cozy Bay."
+    ),
+    c(
+      95,
+      "15th December."
+    ),
+    c(
+      89,
+    ),
+    c(
+      72,
+      "14th December. On the way to Kruger. Recording number 2."
+    )
   );
   bmnh = list(
-    210,
-    208,
-    206,
-    196,
-    130,
-    128,
-    117,
-    52,
-    61,
-    41,
-    26
+    c(210),
+    c(208),
+    c(206),
+    c(196),
+    c(130),
+    c(128),
+    c(117),
+    c(52),
+    c(61),
+    c(41),
+    c(26)
   );
 );
