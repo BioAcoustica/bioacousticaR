975a20b9033643d6fb8c39518b8dd3de4469c078
Quentin Geissmann <qgeissmann@gmail.com>
2016-10-25 17:55:38 +0100

    doc ++ on prototype

--- R/bioacoustica.R
+++ R/bioacoustica.R
@@ -8,7 +8,6 @@ getHandle <- function(url="http://bio.acousti.ca"){
   return(list(url))
 }
 
-
 authenticate <- function(username, password) {
   return(drupalr.authenticate(getHandle(), username, password));  
 }
@@ -38,18 +37,61 @@ fetchView <- function(path, verbose=F) {
 #   return (collections);
 # }
 
+#' Dowload a list of all annotations 
+#' 
+#' Download the information corresponding 
+#' to all available annotaions on bioacoustica
+#' 
+#' @param path the view containing the annotations
+#' @return a \code{data.table}. The column \code{id} refers to the 
+#' unique identifier of an annotation. \code{start} and \code{end} identify the
+#' section of this annotation and \code{file} maps the URL of the raw audio file.
+#' Other annotation data are available as extra columns.
 getAllAnnotationData <- function(path = "/R/annotations") {
   fetchView(path);
 }
 
-downloadBinRange <- function(connection, start_byte, end_byte){
+#' To download a partial binary file (see `curl -r`)
+downloadBinRange <- function(connection, start_byte, end_byte, 
+                             n_retry = 5){
   # this way we can fecth only a part of a binary file
-  opts = curlOptions(range = paste(start_byte, end_byte,sep="-"))
-  raw <- getBinaryURL(connection, .opts = opts)
+  for(i in 1:n_retry){
+    opts <- curlOptions(range = paste(start_byte, end_byte,sep="-"))
+    raw <- getBinaryURL(connection, .opts = opts)
+    expected_length <- end_byte - start_byte + 1
+  
+    if(expected_length == length(raw))
+      return(raw)
+    warning(paste("Failed to retreive", connection, ". Retrying..."))
+  }
+  stop(paste("Did not manage to fetch", connection, "!"))
 }
 
-getAnnotationFile <- function(file, start, end, dst, header_size=44){
-  print (file)
+#' Fetch the portion of a remote wav file
+#' 
+#' This function can download only a portion of a wave file
+#' stored online and store it locally as a new wave file.
+#' It proceeds by dowloading metadata, uses them to locate the file chunk
+#' and reconstitute a file
+#' 
+#' @param file, the URL of a file
+#' @param start the start of the desired section, in second
+#' @param end the end of the desired section, in second
+#' @param dst a character vector indicating where to save the resulting file
+#' @param header_size the number of bytes in the header of a wave file
+#' @param verbose weher to print extra information
+#' @return the path to resulting file
+#' @seealso \code{\link{dowloadFilesForAnnotations}}, a wrapper around this function
+#' @export
+getAnnotationFile <- function(file, 
+                              start, 
+                              end, 
+                              dst, 
+                              header_size=44, 
+                              verbose=F){
+  if(verbose)
+    message(paste("getting", file, "from", start, "to", end))
+  
   header_bin = downloadBinRange(file, 0, header_size)
   header_tmp_file <- tempfile("ba_header_", fileext = ".wav")
   writeBin(header_bin, header_tmp_file)
@@ -95,25 +137,49 @@ getAnnotationFile <- function(file, start, end, dst, header_size=44){
     stop(sprintf("Error whilst reading wav, resulting wav
            does not have expected duration.
             Expected = %f. real = %f",end-start,duration ))
-  # here we fix the wave metadata
+  
+  # here we fix the wave metadata by rewriting it
   writeWave(wav, dst)
   return(dst)
 }
 
-#yo <- "/tmp/test/annotation_246_564_4_Chorthippus_brunneus_766r1.wav"
-#
 
-query = getAllAnnotationData()
-
-q <- query[author == "qgeissmann"]
-
-q[, dst := paste("/tmp/test", paste("annotation",id,basename(file),sep = "_"), sep="/")]
-q[, start := as.numeric(start)]
-q[, end := as.numeric(end)]
-
-qq <- q[, getAnnotationFile(file, start, end, dst=dst), by=id]
-
-
+#' Retreive list of annotation files from a query
+#'
+#' This function uses a formated query
+#' (i.e. table) to retreive annotation raw data
+#' 
+#' @param query a data.table or data.frame (see details)
+#' @param dst_dir a directory to store the resulting data
+#' @param prefix a string to prepend to filenames
+#' @details \code{query} must contain the columns \code{id},
+#' \code{start}, \code{end}, \code{file}. They indicate, the annotation uid
+#' , the start and end of the wav file, and the url of the wav file, respactively.
+#' Typically, it will be obtained through \code{\link{getAllAnnoationData}}.
+#' @seealso \code{\link{getAllAnnoationData}}
+#' @examples
+#' \dontrun{
+#' all_annotations = getAllAnnotationData()
+#' query = all_annotations[author=="qgeissmann"]
+#' dowloadFilesForAnnotations(query, dst_dir = "/tmp/my_annotation")
+#' }
+#' @export
+dowloadFilesForAnnotations <- function(query, 
+                                       dst_dir, 
+                                       prefix="annotation"){
+  if(!dir.exists(dst_dir))
+    stop(paste(dst_dir, "does not exist"))
+  q <- as.data.table(query)
+  q[, dst := paste(dst_dir, paste(prefix,id,basename(file),sep = "_"), sep="/")]
+  q[, start := as.numeric(start)]
+  q[, end := as.numeric(end)]
+  annotation_file_map <- q[, 
+                           .(annotation_path = getAnnotationFile(file, start, end, dst=dst)), 
+                             by=id]
+  setkeyv(q, "id")
+  setkeyv(annotation_file_map, "id")
+  q[annotation_file_map]
+}
 
 # 
 # bioacoustica.postComment <- function(path, body, c) {
