be1d744c2b304b744cdfbd307e1ec4875828d5df
Ed Baker <edwbaker@gmail.com>
2017-03-11 18:07:13 +0000

    Support collections.

---
+++ R/collections.R
@@ -0,0 +1,31 @@
+collectionDownload <- function(collectionID) {
+  
+  collection <- read.csv(text=drupalr.get("bio.acousti.ca/", "collection/csv/17197/17197", c))
+  
+  bioacoustica.mkdir("bioacoustica_data")
+  
+  downloadData <- function(data, name) {
+    url <- data["recording_url"]
+    filename <- paste0(data["entity_id"],".wav")
+    download.file(url, filename)
+    
+    if (data["entity_type"] == "annotation") {
+      trimFile(filename, data["start"], data["end"])
+    }
+  }
+  
+  trimFile <- function(filename, start, end) {
+    long <- readWave(filename)
+    f <- long@samp.rate
+    short <- cutw(long, f=f, from=as.numeric(start),to=as.numeric(end), method="Wave")
+    file.remove(filename)
+    savewav(short, f=f, file=filename)
+  }
+  
+  urls <- as.character(collection[,"recording_url"])
+  filenames <- as.character(collection[,"entity_id"])
+  setwd("./bioacoustica_data")
+  apply(collection, 1, downloadData)
+  setwd("..")
+  
+}
