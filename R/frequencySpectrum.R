98c99cbbf3ae2dbef1eaf00c90be1a737d6f9dd0
Ed Baker <edwbaker@gmail.com>
2017-03-12 15:36:51 +0000

    Frequency domain stuff.

--- R/frequencySpectrum.R
+++ R/frequencySpectrum.R
@@ -1,8 +1,8 @@
-dominantFrequency <- function(filename, min_cutoff=1) {
+dominantFrequency <- function(filename) {
   filename <- paste0(filename,".wav")
   song <- readWave(paste0("bioacoustica_data/",filename))
   f <- song@samp.rate
   data <- meanspec(song, f=f, plot=FALSE)
-  data <- data[which(data[,1] > min_cutoff),]
-  return(data[[which.max(data[,2]),1]])
+  data <- data[which(data[,2] > mean(ms[,2])),]
+  return(c(min(data[,1]),"-",max(data[,1]))
 }
