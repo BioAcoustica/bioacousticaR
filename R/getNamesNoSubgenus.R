7256ef225ab72f5d77803b749155f53d492829ec
Ed Baker <edwbaker@gmail.com>
2017-03-14 20:45:42 +0000

    Get taxa names without subgenera.

---
+++ R/getNamesNoSubgenus.R
@@ -0,0 +1,7 @@
+getNamesNoSubgenus <- function(c) {
+  ba_names <- unique(read.csv(text = drupalr.get("http://bio.acousti.ca/", "aao/orthoptera", c)))
+  ba_subspecies <- unique(read.csv(text = drupalr.get("http://bio.acousti.ca/", "aao/orthoptera/subspecies", c)))
+  ba_names <- rbind(ba_names, ba_subspecies)
+  ba_names$SupertreeTaxon <- gsub(' ', '_', ba_names$SupertreeTaxon)
+  return(ba_names)
+}
